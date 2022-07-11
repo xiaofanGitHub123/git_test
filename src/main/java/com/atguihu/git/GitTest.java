a01c03e772bcc5308e1f7fdd1424e0caa1ff5062
xiaofan <1837658828@qq.com>
2022-07-11 15:08:48 +0800

    commit1222

--- src/main/java/com/atguihu/git/GitTest.java
+++ src/main/java/com/atguihu/git/GitTest.java
@@ -11,8 +11,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello22");
         System.out.println("hello2");
-        System.out.println("hello2");System.out.println("hello2");
+        System.out.println("hello2");
 
     }
 }
