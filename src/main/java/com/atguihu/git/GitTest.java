d4b8465532713cc1290c0fcb4cb86d5b40788630
xiaofan <18252705106@qq.com>
2022-07-11 13:55:14 +0800

    commit

---
+++ src/main/java/com/atguihu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguihu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author fan
+ * @Date 2022/7/11 13:49
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
